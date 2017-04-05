5a0a4e437c8192c090876ce1bbb7165b6ba5131e
Hzwcode <1359434736@qq.com>
2017-04-05 18:11:32 +0800

    add background mouse click animation

---
+++ themes/yelee/source/resources/typewriter.js
@@ -0,0 +1,1 @@
+!function(t){t.fn.typewriter=function(){return this.each(function(){var n=t(this),r=n.html(),e=0;n.html("");var i=setInterval(function(){var t=r.substr(e,1);"<"==t?e=r.indexOf(">",e)+1:e++,n.html(r.substring(0,e)+(1&e?"_":"")),e>=r.length&&clearInterval(i)},150)}),this}}(jQuery),$("#code").show().typewriter();
